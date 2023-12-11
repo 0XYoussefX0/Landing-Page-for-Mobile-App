747f732d9365506dbd77e6b04655074738b31ac3
Youssef Ab <youssef.abelouass@gmail.com>
2023-12-11 09:34:50 +0100

    commented code because it triggers an error in the deployment phase

--- functions/googlePlayScrape/index.js
+++ functions/googlePlayScrape/index.js
@@ -27,11 +27,11 @@ export async function fetchPlayStoreReviews(req, res) {
       }
     })
 
-    await axios.post("secondFunctionUrl", JSON.stringify(reviews), {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
+    // await axios.post("secondFunctionUrl", JSON.stringify(reviews), {
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    // })
     res.status(200).send("Reviews fetched and sent successfully!")
   } catch (error) {
     console.log("Error scraping reviews in Google Play:", error)
